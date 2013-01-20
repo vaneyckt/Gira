72b281535ea93dec264b78122449eb19a6961f67
Tom Van Eyck <tom.vaneyck@swrve.com>
2013-01-20 17:26:08 +0000

    improving code

--- gira.rb
+++ gira.rb
@@ -1,16 +1,31 @@
 require './lib/github.rb'
+require './lib/helpers.rb'
 
-module Gira
-  while true
-    begin
-      pull_request_events = Github.get_pull_request_events_after(nil)
-      pull_request_events.each do |event|
-        puts events.to_s
-        puts "\n"
-      end
-    rescue => e
-      Logger.log('Error in main loop', e)
-    end
-    sleep config[:github_polling_interval_seconds]
+def on_pull_request_open(event)
+  puts "opened pull request #{event.payload.number}"
+end
+
+def on_pull_request_merge(event)
+  puts "merged pull request #{event.payload.number}"
+end
+
+def on_pull_request_close(event)
+  puts "closed pull request #{event.payload.number}"
+end
+
+
+timestamp = nil
+
+while true
+  begin
+    config = ConfigFile.read
+    data = Github.get_pull_request_info_after(timestamp)
+    data[:opened_pull_request_events].each { |event| on_pull_request_open(event) }
+    data[:merged_pull_request_events].each { |event| on_pull_request_merge(event) }
+    data[:closed_pull_request_events].each { |event| on_pull_request_close(event) }
+    timestamp = data[:last_event_timestamp]
+  rescue => e
+    Logger.log('Error in main loop', e)
   end
+  sleep config[:github_polling_interval_seconds]
 end
--- lib/github.rb
+++ lib/github.rb
@@ -1,42 +1,46 @@
 require 'octokit'
-require '/lib/helpers.rb'
+require './lib/helpers.rb'
 
-module Gira
-  module Github
-    def Github.get_pull_request_events_after(event_id)
-      events = get_repository_events_after(event_id)
-      pull_request_events = events.select { |event| event.type == 'PullRequestEvent' }
-      {:last_event_id => events.last.id, :pull_request_events => pull_request_events}
-    end
+module Github
+  def Github.get_pull_request_info_after(timestamp)
+    events = get_repository_events_after(timestamp)
+    pull_request_events = events.select { |event| event.type == 'PullRequestEvent' }
 
-    def Github.get_repository_events_after(event_id)
-      events = []
-      finished = false
+    result = {}
+    result[:last_event_timestamp] = events.last.created_at
+    result[:opened_pull_request_events] = pull_request_events.select { |event| event.payload.action == 'opened' || event.payload.action == 'reopened'}
+    result[:merged_pull_request_events] = pull_request_events.select { |event| event.payload.action == 'closed' && event.payload.pull_request.merged == true }
+    result[:closed_pull_request_events] = pull_request_events.select { |event| event.payload.action == 'closed' && event.payload.pull_request.merged == false }
+    result
+  end
 
-      (1..10).each do |page|
-        if !finished
-          page_events = get_repository_events(:page => page)
-          page_events.each do |page_event|
-            finished = finished || (page_event.id == event_id)
-            events << page_event if !finished
-          end
-          sleep 5
+  def Github.get_repository_events_after(timestamp)
+    events = []
+    finished = false
+
+    (1..10).each do |page|
+      if !finished
+        page_events = get_repository_events(:page => page)
+        page_events.each do |page_event|
+          finished ||= !timestamp.nil? and page_event.created_at <= timestamp
+          events << page_event if !finished
         end
-      end
-      events.sort { |x,y| x.created_at <=> y.created_at }
-    end
-
-    def Github.get_repository_events(options = {})
-      begin
-        config = ConfigFile.read
-        respository_id = Repository.get_id
-        client = Octokit::Client.new(:login => config[:github_login], :password => config[:github_password])
-        client.repository_events(respository_id, options)
-      rescue => e
-        Logger.log('Error when trying to get repository events', e)
         sleep 5
-        retry
       end
+    end
+    events.sort { |x,y| x.created_at <=> y.created_at }
+  end
+
+  def Github.get_repository_events(options = {})
+    begin
+      config = ConfigFile.read
+      respository_id = Repository.get_id
+      client = Octokit::Client.new(:login => config[:github_login], :password => config[:github_password])
+      client.repository_events(respository_id, options)
+    rescue => e
+      Logger.log('Error when trying to get repository events', e)
+      sleep 5
+      retry
     end
   end
 end
