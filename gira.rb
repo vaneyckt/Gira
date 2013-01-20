718eb7267b4581b92e8181adbb103939ac77097c
Tom Van Eyck <tom.vaneyck@swrve.com>
2013-01-20 20:57:12 +0000

    rename variable

--- gira.rb
+++ gira.rb
@@ -19,11 +19,11 @@ timestamp = nil
 while true
   begin
     config = ConfigFile.read
-    data = Github.get_pull_request_info_after(timestamp)
-    data[:opened_pull_request_events].each { |event| on_pull_request_open(event) }
-    data[:merged_pull_request_events].each { |event| on_pull_request_merge(event) }
-    data[:closed_pull_request_events].each { |event| on_pull_request_close(event) }
-    timestamp = data[:last_event_timestamp]
+    info = Github.get_pull_request_info_after(timestamp)
+    info[:opened_pull_request_events].each { |event| on_pull_request_open(event) }
+    info[:merged_pull_request_events].each { |event| on_pull_request_merge(event) }
+    info[:closed_pull_request_events].each { |event| on_pull_request_close(event) }
+    timestamp = info[:last_event_timestamp]
   rescue => e
     Logger.log('Error in main loop', e)
   end
